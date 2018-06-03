47c424de802e8c2d70a35f017deeeb9571ad03bf
sulvto <sulvto@gmail.com>
2018-06-03 13:49:38 +0800

    Add debug

---
+++ crowbar/debug/debug.h
@@ -0,0 +1,13 @@
+//
+// Created by sulvto on 18-6-3.
+//
+
+#ifndef CROWBAR_DEBUG_H
+#define CROWBAR_DEBUG_H
+
+struct DBG_Controller_tag {
+    FILE    *debug_write_fp;
+    int     current_debug_level;
+};
+
+#endif //CROWBAR_DEBUG_H
