f49168d525778e0bdaa9c3d3ddc796368f4c43bf
sulvto <sulvto@gmail.com>
2018-06-13 19:23:59 +0800

    Add debug

---
+++ diksam/debug/debug.h
@@ -0,0 +1,15 @@
+//
+// Created by sulvto on 18-6-13.
+//
+
+#ifndef DIKSAM_DEBUG_H
+#define DIKSAM_DEBUG_H
+
+#include "DBG.h"
+
+struct DBG_Controller_tag {
+    FILE    *debug_write_fp;
+    int     current_debug_level;
+};
+
+#endif //DIKSAM_DEBUG_H
